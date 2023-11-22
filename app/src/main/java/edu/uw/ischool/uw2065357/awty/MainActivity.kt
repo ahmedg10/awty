669db83b61401f684b909f973d123ed724cec59c
Ahmed Ghaddah <ahmedghaddah@ahmeds-air.lan>
2023-11-21 22:37:43 -0800

    attempt at the extra credit

--- app/src/main/java/edu/uw/ischool/uw2065357/awty/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/uw2065357/awty/MainActivity.kt
@@ -2,130 +2,249 @@ package edu.uw.ischool.uw2065357.awty
 
 import android.app.AlarmManager
 import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.SystemClock
-import android.telephony.PhoneNumberFormattingTextWatcher
+import android.telephony.SmsManager
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.util.Log
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
-
+    companion object {
+        const val ALARM_ACTION = "edu.uw.ischool.uw2065357.awty.ALARM"
+    }
     private lateinit var phoneNumber: EditText
     private lateinit var message: EditText
     private lateinit var interval: EditText
     private lateinit var startBtn: Button
-    private lateinit var alarmManager: AlarmManager
-
-    // Add this variable to the class to store handlers for each phone number
-    private val handlerMap = HashMap<String, Handler>()
-    private val handler = Handler(Looper.getMainLooper())
+    private var alarmManager: AlarmManager? = null
+    private var pendingIntent: PendingIntent? = null
+    private var receiver: BroadcastReceiver? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Rename variables to match the original code
         phoneNumber = findViewById(R.id.editPhoneNumber)
         message = findViewById(R.id.editMessage)
         interval = findViewById(R.id.editInterval)
         startBtn = findViewById(R.id.btnStartStop)
 
-        // Use the renamed variables and functions
-        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-
-        phoneNumber.addTextChangedListener(PhoneNumberFormattingTextWatcher())
+        setupTextWatchers()
 
         startBtn.setOnClickListener {
-            if (isValidPhone(phoneNumber) && isValidInterval(interval)) {
-                val numMessage = formatPhoneNumber(phoneNumber.text.toString())
-                val numInterval = interval.text.toString().toInt()
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.SEND_SMS), 101)
+            } else {
+                toggleNag()
+            }
+        }
+    }
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent?.let {
+            when (it.getStringExtra("task")) {
+                "sendAudioMMS" -> sendAudioMMS(it.getStringExtra("phoneNumber") ?: "")
+                "sendVideoMMS" -> sendVideoMMS(it.getStringExtra("phoneNumber") ?: "")
+            }
+        }
+    }
 
-                val intent = Intent("edu.uw.ischool.uw2065357.awty")
-                intent.putExtra("message", message.text.toString())
-                intent.putExtra("phone", numMessage)
+    // I could not figure out how to implement this gave my best shot and filed with a dummy file
 
-                val pendingIntent = PendingIntent.getBroadcast(
-                    this,
-                    0,
-                    intent,
-                    PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE
-                )
-                val intentFilter = IntentFilter("edu.uw.ischool.uw2065357.awty")
-                registerReceiver(AlarmReceiver(), intentFilter)
+    private fun sendAudioMMS(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "audio/*" // MIME type for audio
+            putExtra("address", phoneNumber)
+            putExtra(Intent.EXTRA_STREAM, Uri.parse("file://path/to/your/audio/file")) // Path to your audio file
+        }
+        startActivity(Intent.createChooser(intent, "Send Audio MMS..."))
+    }
 
-                if (startBtn.text == "Start") {
-                    alarmManager.setRepeating(
-                        AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                        SystemClock.elapsedRealtime() + numInterval.toLong() * (60 * 1000),
-                        numInterval.toLong() * (60 * 1000),
-                        pendingIntent
-                    )
 
-                    showToastDuringInterval(
-                        numInterval * 60 * 1000,
-                        numMessage,
-                        message.text.toString()
-                    )
+    // I could not figure out how to implement this gave my best shot and filed with a dummy file
+    private fun sendVideoMMS(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "video/*" // MIME type for video
+            putExtra("address", phoneNumber)
+            putExtra(Intent.EXTRA_STREAM, Uri.parse("file://path/to/your/video/file")) // Path to your video file
+        }
+        startActivity(Intent.createChooser(intent, "Send Video MMS..."))
+    }
 
-                    startBtn.text = "Stop"
-                } else {
-                    startBtn.text = "Start"
-                    alarmManager.cancel(pendingIntent)
-                    pendingIntent.cancel()
+
+    private fun setupTextWatchers() {
+        val phoneTextWatcher = object : TextWatcher {
+            private var isFormatting = false
+            private var oldStr = ""
+
+            override fun afterTextChanged(s: Editable?) {
+                if (isFormatting || s == null) return
+
+                isFormatting = true
+
+                val newStr = formatPhoneNumber(s.toString())
+                s.replace(0, s.length, newStr)
+
+                isFormatting = false
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                oldStr = s.toString()
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        }
+
+        phoneNumber.addTextChangedListener(phoneTextWatcher)
+
+        val generalTextWatcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val phoneValid = phoneNumber.text.isNotEmpty() && isValidPhoneNumber(phoneNumber.text.toString())
+                val messageValid = message.text.isNotEmpty()
+                val intervalValid = interval.text.isNotEmpty() && interval.text.toString().toIntOrNull() != null
+
+                startBtn.isEnabled = phoneValid && messageValid && intervalValid
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        }
+
+        message.addTextChangedListener(generalTextWatcher)
+        interval.addTextChangedListener(generalTextWatcher)
+    }
+
+    private fun formatPhoneNumber(phone: String): String {
+        // Remove all non-digit characters
+        val digits = phone.filter { it.isDigit() }
+
+        // Format based on the length of the digit-only string
+        return when {
+            digits.length >= 6 -> "(${digits.substring(0, 3)})${digits.substring(3, 6)}-${digits.substring(6)}"
+            digits.length > 3 -> "(${digits.substring(0, 3)})${digits.substring(3)}"
+            else -> "($digits"
+        }
+    }
+
+
+    private fun toggleNag() {
+        Log.d("NagFunction", "toggleNag called. PendingIntent is ${if (pendingIntent == null) "null" else "not null"}")
+        if (pendingIntent == null) {
+            startNag()
+        } else {
+            stopNag()
+        }
+    }
+
+    private fun startNag() {
+        Log.d("NagFunction", "Starting Nag")
+        startBtn.text = "Stop"
+
+        // Register the BroadcastReceiver if it hasn't been registered yet
+        if (receiver == null) {
+            receiver = SmsBroadcastReceiver()
+            val filter = IntentFilter(ALARM_ACTION)
+            registerReceiver(receiver, filter)
+        }
+
+        // Create an Intent with the custom action
+        val intent = Intent(ALARM_ACTION).apply {
+            putExtra("phoneNumber", phoneNumber.text.toString())
+            putExtra("message", message.text.toString())
+        }
+
+        // Create the PendingIntent with the custom action intent
+        val alarmIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        pendingIntent = alarmIntent
+
+        // Get the alarm manager and set the repeating alarm
+        alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intervalMillis = interval.text.toString().toLong() * 60000 // Convert minutes to milliseconds
+
+        alarmManager?.setRepeating(
+            AlarmManager.RTC_WAKEUP,
+            System.currentTimeMillis(),
+            intervalMillis,
+            alarmIntent
+        )
+
+        Log.d("NagFunction", "Alarm Set for every $intervalMillis milliseconds")
+    }
+
+
+    private fun stopNag() {
+        Log.d("NagFunction", "Stopping Nag")
+        startBtn.text = "Start"
+        pendingIntent?.let {
+            alarmManager?.cancel(it)
+            it.cancel()
+        }
+        pendingIntent = null
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 101 && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            toggleNag()
+        }
+    }
+
+    override fun onDestroy() {
+        receiver?.let { unregisterReceiver(it) }
+        super.onDestroy()
+        stopNag()
+    }
+
+    private fun isValidPhoneNumber(phone: String): Boolean {
+        // Regex for phone number in format (XXX)XXX-XXXX
+        val phoneRegex = Regex("\\(\\d{3}\\)\\d{3}-\\d{4}")
+        return phone.matches(phoneRegex)
+    }
+}
+
+class SmsBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val phoneNumber = intent?.getStringExtra("phoneNumber") ?: return
+        val message = intent?.getStringExtra("message") ?: return
+        val messageType = intent.getStringExtra("messageType") ?: "sms"
+
+        if (context != null) {
+            when (messageType) {
+                "sms" -> {
+                    Log.d("SmsBroadcastReceiver", "Sending SMS to $phoneNumber: $message")
+                    val smsManager = SmsManager.getDefault()
+                    smsManager.sendTextMessage(phoneNumber, null, message, null, null)
+                }
+
+                "audio", "video" -> {
+                    val newIntent = Intent(context, MainActivity::class.java).apply {
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        putExtra(
+                            "task",
+                            if (messageType == "audio") "sendAudioMMS" else "sendVideoMMS"
+                        )
+                        putExtra("phoneNumber", phoneNumber)
+                    }
+                    context.startActivity(newIntent)
                 }
             }
         }
     }
-
-    private fun isValidInterval(interval: EditText): Boolean {
-        return !(interval.text.toString().toInt() == 0 || interval.text.toString().toInt() < 0 || interval.text.toString() == "")
-    }
-
-    private fun isValidPhone(phoneNumber: EditText): Boolean {
-        return !(phoneNumber.text.toString() == "" || phoneNumber.text.toString().length != 14)
-    }
-
-    private fun formatPhoneNumber(number: String): String {
-        return "(${number.substring(0, 3)}) ${number.substring(3, 6)}-${number.substring(6, 10)}"
-    }
-
-    private fun showToastDuringInterval(intervalMillis: Int, phoneNumber: String, message: String) {
-        val showToastRunnable = object : Runnable {
-            override fun run() {
-                // Create custom Toast
-                val toastView = layoutInflater.inflate(R.layout.custom_toast, null)
-                val toastCaption = toastView.findViewById<TextView>(R.id.toastCaption)
-                val toastMessage = toastView.findViewById<TextView>(R.id.toastMessage)
-
-                toastCaption.text = "Texting $phoneNumber"
-                toastMessage.text = message
-
-                val toast = Toast(applicationContext)
-                toast.duration = Toast.LENGTH_SHORT
-                toast.view = toastView
-                toast.show()
-
-                handler.postDelayed(this, intervalMillis.toLong())
-            }
-        }
-
-        handler.postDelayed(showToastRunnable, intervalMillis.toLong())
-    }
-
-    // Override onDestroy to remove all callbacks
-    override fun onDestroy() {
-        super.onDestroy()
-        for (handler in handlerMap.values) {
-            handler.removeCallbacksAndMessages(null)
-        }
-    }
 }
+
 
